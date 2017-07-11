fd820d22fb4d94b96a286b7f36e44af9a86e10b9
Kari <karinclimber@gmail.com>
2017-07-10 20:42:34 -0400

    working almost done

--- assets/javascript/triviabasic.js
+++ assets/javascript/triviabasic.js
@@ -1,11 +1,9 @@
 $("#startButton").on("click", function() {
-  $("#startButton").remove();
-  for (var i=0; i < questions.length; i++) {
-    $("#subWrapper").append("<h2>"+questions[i].question+"</h2>");
-      for (var j=0; j < questions[i].answers.length; j++) {
-        $("#subWrapper").append("<input type='radio' name='question-"+i+"' value='+questions[i].answers[j]+'>"+questions[i].answers[j]);
-      }
-  }
+  play.start();
+})
+
+$(document).on("click", "#submit", function(){
+  play.done();
 })
 
 // questions array
@@ -52,120 +50,109 @@ var questions = [{
   }
   ];
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var questions = new Array();
-// questions[0] = ["What does Honolulu translate to in English?"];
-// questions[1] = ["What is the capital of Hungary?"];
-
-// var choices = new Array();
-// choices[0] = ["Monkeys", "Hello", "Welcome", "Sheltered Harbor"];
-// choices[1] = ["Tanzania", "Budapest", "Israel", "Cantha"];
-
-// var answers = new Array();
-// answers[0] = ["Sheltered Harbor"];
-// answers[1] = ["Budapest"];
-
-// $("#titleDiv").html("TRIVIA");
-
-//    //  Interval Demonstration
-//     //  Set our number counter to 100.
-//     var number = 10;
-
-//     //  Variable that will hold our interval ID when we execute
-//     //  the "run" function
-//     var intervalId;
-//     $("#timeRemainingDiv").html("Time Remaining: " + number);
-    
-
-//     //  The run function sets an interval
-//     //  that runs the decrement function once a second.
-//     function run() {
-//       intervalId = setInterval(decrement, 1000);
-//     }
-
-//     //  The decrement function.
-//     function decrement() {
-
-//       //  Decrease number by one.
-//       number--;
-
-//       //  Once number hits zero...
-//       if (number === 0) {
-
-//         //  ...run the stop function.
-//         stop();
-
-//         //  Alert the user that time is up.
-//         alert("Time Up!");
-//       }
-//     }
-
-//     //  The stop function
-//     function stop() {
-
-//       //  Clears our intervalId
-//       //  We just pass the name of the interval
-//       //  to the clearInterval function.
-//       clearInterval(intervalId);
-//     }
-
-//     //  Execute the run function.
-//     run();
-
-// //    load the questions
-//     window.onload = askQuestion;
-//     var askQuestion = function() {
-//      var randomQuestion = questions[Math.floor(Math.random() *questions.length)];
-//         $("#questionsDiv").html(randomQuestion);
-//     var i = questions.indexOf(randomQuestion);
-//     console.log(i);    
-//     console.log(randomQuestion)      
-
-//  // show the answer buttons   
-//     var answerButton = choices[i];
-//      for (var j = 0; j < choices[i].length; j++) {
-//         var answerBtn = $("<button>");
-//         answerBtn.addClass("btn btn-primary");
-//         answerBtn.attr("data-answers", "choices[i][j]");
-//         answerBtn.text(choices[i][j]);
-//         $("#choicesDiv").append(answerBtn);
-//      }
-//    // make the buttons save the answer to a variable 
-//     $(".btn").on("click", function(event) {
-//         var userChoice = $(this)[0].innerText;
-//         console.log(userChoice)
-        
-
-    
-//     })  
-// // check if button click == correct answer
-// function check() {
-// //         check(userChoice);
-// //         if (userChoice === correctAnswer[i]) {
-// //             console.log("yes");
-// //         }
-// //         else {
-// //             console.log("wrong")
-// //         }
-// //         console.log(answerButton)    
-// }
-       
-//     askQuestion();  
-   
+  var play = {
+    correct: 0,
+    incorrect: 0,
+    counter: 25,
+    countdown: function() {
+      play.counter--;
+      $("#counter").html(play.counter);
+      if(play.counter<=0){
+        console.log("Time is up");
+        play.done();
+      }
+    },
+    start: function() {
+      timer = setInterval(play.countdown, 1000);
+      $("#subWrapper").prepend("<h2>Time Remaining: <span id='counter'>25</span> Seconds</h2>");
+        $("#startButton").remove();
+    for (var i=0; i < questions.length; i++) {
+      $("#subWrapper").append("<h2>"+questions[i].question+"</h2>");
+        for (var j=0; j < questions[i].answers.length; j++) {
+          $("#subWrapper").append("<input type='radio' name='question-"+i+"' value='+questions[i].answers[j]+'>"+questions[i].answers[j]);
+      }
+     }
+     $("#subWrapper").append("<br><br><button class='btn btn-warning' id='submit'>SUBMIT</button>");
+    }, 
+    done: function() {
+      $.each($("input[name='question-0']:checked"), function(){
+        if($(this).val() == questions[0].correctAnswer) {
+          play.correct++;
+          } else {
+            play.incorrect++;
+          }
+      });
+      $.each($("input[name='question-1']:checked"), function(){
+        if($(this).val() == questions[1].correctAnswer) {
+        play.correct++;
+        } else {
+          play.incorrect++;
+        }
+      });
+      $.each($("input[name='question-2']:checked"), function(){
+        if($(this).val() == questions[2].correctAnswer) {
+        play.correct++;
+        } else {
+          play.incorrect++; 
+        }
+      });
+      $.each($("input[name='question-3']:checked"), function(){
+        if($(this).val() == questions[3].correctAnswer) {
+        play.correct++;
+        } else {
+          play.incorrect++;
+        }
+      });
+      $.each($("input[name='question-4']:checked"), function(){
+        if($(this).val() == questions[4].correctAnswer) {
+        play.correct++;
+        } else {
+          play.incorrect++; 
+        }
+      });
+      $.each($("input[name='question-5']:checked"), function(){
+        if($(this).val() == questions[5].correctAnswer) {
+          play.correct++;
+          } else {
+            play.incorrect++;
+          }
+      });
+      $.each($("input[name='question-6']:checked"), function(){
+        if($(this).val() == questions[6].correctAnswer) {
+        play.correct++;
+        } else {
+          play.incorrect++;
+        }
+      });
+      $.each($("input[name='question-7']:checked"), function(){
+        if($(this).val() == questions[7].correctAnswer) {
+        play.correct++;
+        } else {
+          play.incorrect++; 
+        }
+      });
+      $.each($("input[name='question-8']:checked"), function(){
+        if($(this).val() == questions[8].correctAnswer) {
+        play.correct++;
+        } else {
+          play.incorrect++;
+        }
+      });
+      $.each($("input[name='question-9']:checked"), function(){
+        if($(this).val() == questions[9].correctAnswer) {
+        play.correct++;
+        } else {
+          play.incorrect++; 
+        }       
+      });
+      this.result();
+    },
+      result: function(){
+        clearInterval(timer);
+        $("#subWrapper h2").remove();
+        $("#subWrapper").html("<h2>Finished!!!!!!</h2>");
+        $("#subWrapper").append("<h3>Correct Answers: "+this.correct+"</h3>");
+        $("#subWrapper").append("<h3>Incorrect Answers: "+this.incorrect+"</h3>");
+        $("#subWrapper").append("<h3>Questions You Forgot: "+(questions.length-(this.correct+this.incorrect))+"</h3>");
+          }
+    };
